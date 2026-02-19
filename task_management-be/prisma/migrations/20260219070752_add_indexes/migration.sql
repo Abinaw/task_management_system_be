66c0244ab8bb68cef169a890d6de9c02fc1080d2
Abinaw <dabinawan@gmail.com>
2026-02-19 13:42:07 +0530

    [NO TICKET] added indexing to the DB

---
+++ task_management-be/prisma/migrations/20260219070752_add_indexes/migration.sql
@@ -0,0 +1,11 @@
+-- CreateIndex
+CREATE INDEX "Task_userId_idx" ON "Task"("userId");
+
+-- CreateIndex
+CREATE INDEX "Task_status_idx" ON "Task"("status");
+
+-- CreateIndex
+CREATE INDEX "Task_priority_idx" ON "Task"("priority");
+
+-- CreateIndex
+CREATE INDEX "Task_status_priority_idx" ON "Task"("status", "priority");
